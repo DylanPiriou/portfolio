a649096718755999a47f844f817d2c75f75209c8
DylanPiriou <dylan.piriou@hotmail.com>
2023-04-17 00:04:23 +0200

    404 page done

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import "./NotFound.scss";
+import { NavLink } from "react-router-dom";
+import Navbar from '../../Components/Navbar/Navbar';
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="notfound">
+      <Navbar/>
+      <div className="content-container">
+        <h2>404</h2>
+        <p>Oops... Something went wrong 😅</p>
+        <NavLink to="/">Go back home</NavLink>
+      </div>
+    </div>
   )
 }
